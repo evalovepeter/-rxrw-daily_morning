5093595d4229516bf01203222c2a93628a61b781
evalovepeter <92249646+evalovepeter@users.noreply.github.com>
2023-09-03 23:43:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
-from datetime import date, datetime
+import os
 import math
-from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
-import requests
-import os
 import random
+import requests
+from datetime import date, datetime
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage
 
-import os
-os.environ["START_DATE"] = "2021-09-04"
-export CITY=Guangdong
-import os
-os.environ["BIRTHDAY"] = "your_birthday_here"
-export APP_ID=wxef93e5418eed02a4
-export APP_SECRET=cea2184873f7e2d05080bebaaec0eb2f
-export USER_ID=ogh5L67BJwHb0nEacCUcQnIivfQ0
-export TEMPLATE_ID=pklk-W8tsXcZpNBC-kR8aytKksdad95U4O4uvADQ2WY
-
-today = datetime.now()
+birthday = os.environ['BIRTHDAY']
+app_secret = os.environ['APP_SECRET']
+user_id = os.environ['USER_ID']
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
-
 app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
-
-user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
-
+weather_url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
+  res = requests.get(weather_url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
+  today = datetime.now()
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+  today = datetime.now()
+  next_birthday = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next_birthday < today:
+    next_birthday = next_birthday.replace(year=next_birthday.year + 1)
+  return (next_birthday - today).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
@@ -53,9 +41,7 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
